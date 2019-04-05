20b84b3ab4bda1c27ed63eac79364f474afb026e
aowei <aowei@aifocus.ai>
2019-04-05 23:00:01 +0800

    .test

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var router = express.Router();
 
 // 中间件
 
+// 使用模版引擎
+app.engine('html', ejs.__express);
+app.set('view engine','html');
+
+// parse body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
-  res.end('just test');
+  res.render(path.resolve(__dirname, 'src/views/index'));
 });
 
 app.listen('8086');
